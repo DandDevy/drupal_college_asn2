bd88989dd3af075a9402d0053ec16064161c85b7
Daniel Ashcroft <dashcroft4@gmail.com>
2019-11-29 13:09:46 +0000

    calculator done

--- r00168428/modules/simplecalculator/src/Plugin/Block/simplecalculatorBlock.php
+++ r00168428/modules/simplecalculator/src/Plugin/Block/simplecalculatorBlock.php
@@ -21,16 +21,36 @@ class simplecalculatorBlock extends BlockBase {
 <!DOCTYPE html>
 <html>
 <body>
-<input id = "t1"> 
-<button type="button" onclick="myFunction()">Show My Name</button>
-<p id="results"></p>
-<img src="/modules/simplecalculator/images/hat.jpg" alt="Home" />
+
+<button type="button" onclick="concatenate(1)">1</button>
+<button type="button" onclick="concatenate(2)">2</button>
+<button type="button" onclick="concatenate(3)">3</button>
+<button type="button" onclick="concatenate(4)">4</button>
+<button type="button" onclick="concatenate(5)">5</button>
+<button type="button" onclick="concatenate(6)">6</button>
+<button type="button" onclick="concatenate(7)">7</button>
+<button type="button" onclick="concatenate(8)">8</button>
+<button type="button" onclick="concatenate(9)">9</button>
+<button type="button" onclick="concatenate(0)">0</button>
+<button type="button" onclick={concatenate("*")}>*</button>
+<button type="button" onclick={concatenate("+")}>+</button>
+<button type="button" onclick="execute()">=</button>
+
+<p id="results">results</p>
+
 <script>
-function myFunction() {
-  var x = document.getElementById("t1");
-    document.getElementById("results").innerHTML = "My Name is: "
-    + x.value ;
-}
+
+var calculation = ""
+function concatenate(i){
+        calculation += i;
+        document.getElementById("results").innerHTML = calculation;
+		}
+
+function execute() {
+        const res = eval(calculation);
+        calculation = "";
+        document.getElementById("results").innerHTML = res;
+    }
 </script>
 </body>
 </html>
