28829d8d5a5c4fae0cf9d509ed8143faefd513b7
Daniel Ashcroft <dashcroft4@gmail.com>
2019-12-01 11:15:46 +0000

    modified farshads but didn't work

---
+++ r00168428/modules/tableautoincrementid/src/Plugin/Block/others.php
@@ -0,0 +1,25 @@
+<?php
+namespace Drupal\farshad\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+class others {
+function table_exists( $table)
+{
+ $database = \Drupal::database();
+    	
+    try {
+        $database->query("SELECT 1 FROM {" . $table . "}");
+         return true;
+        } 
+    catch (\Exception $e) {
+         return false;
+         }
+   }
+}
+
+
+
+
+
+
+
