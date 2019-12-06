5f64339c7438e8d61e386918ff353f103208c86f
Daniel Ashcroft <dashcroft4@gmail.com>
2019-12-06 20:32:17 +0000

    recorded the 2 videos

--- r00168428/modules/videoplayerforsetvideos/src/Plugin/Block/videoplayerforsetvideosBlock.php
+++ r00168428/modules/videoplayerforsetvideos/src/Plugin/Block/videoplayerforsetvideosBlock.php
@@ -15,8 +15,8 @@ class videoplayerforsetvideosBlock extends BlockBase {
      * {@inheritdoc}
      */
      public function build() {
-      
-         
+
+
         $markup = Markup::create('
 <!DOCTYPE html>
 <html>
@@ -46,6 +46,39 @@ class videoplayerforsetvideosBlock extends BlockBase {
 
     }
 </script>
+
+
+
+
+<!--<form>-->
+<!--    Enter embedded video source link (Do not put in the full iframe tag, just src value)<br>-->
+<!--    <input type="text" id="userVideo" >-->
+<!--</form>-->
+<!--<button type="button" onclick={setVideoByUser("play")}>play</button>-->
+<!--<button type="button" onclick={setVideoByUser("")}>close</button>-->
+<!--<section id="videoSection2"></section>-->
+
+
+<!--<script>-->
+<!--    function setVideoByUser(value) {-->
+<!--        if(value === "")-->
+<!--            document.getElementById("videoSection").innerHTML = "";-->
+<!--        else{-->
+<!--            const userchoice = document.getElementById("userVideo").value;-->
+<!--            let video_id = userchoice.split(\'v=\')[1];-->
+<!--            let ampersandPosition = video_id.indexOf(\'&\');-->
+<!--            if(ampersandPosition != -1) {-->
+<!--                video_id = video_id.substring(0, ampersandPosition);-->
+<!--            }-->
+<!--            const videoEmbeddedLink = "https://www.youtube.com/embed/" + video_id;-->
+
+<!--            const res = "<iframe width=\"400\" height=\"300\" src=\" " + videoEmbeddedLink + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"-->
+
+<!--            document.getElementById("videoSection2").innerHTML = res;-->
+<!--        }-->
+
+<!--    }-->
+<!--</script>-->
 </body>
 </html>
 
--- r00168428/modules/videoplayerforuserpickedvideo/src/Plugin/Block/videoplayerforuserpickedvideoBlock.php
+++ r00168428/modules/videoplayerforuserpickedvideo/src/Plugin/Block/videoplayerforuserpickedvideoBlock.php
@@ -15,31 +15,37 @@ class videoplayerforuserpickedvideoBlock extends BlockBase {
      * {@inheritdoc}
      */
      public function build() {
-      
-         
+
+
         $markup = Markup::create('
 <!DOCTYPE html>
 <html>
 <body>
 <form>
-    Enter embedded video source link (Do not put in the full iframe tag, just src value)<br>
+    Enter youtube video link :<br>
     <input type="text" id="userVideo" >
 </form>
-<button type="button" onclick={setVideo("play")}>play</button>
-<button type="button" onclick={setVideo("")}>close</button>
-<section id="videoSection"></section>
+<button type="button" onclick={setVideoByUser("play")}>play</button>
+<button type="button" onclick={setVideoByUser("")}>close</button>
+<section id="videoSection2"></section>
 
 
 <script>
-    document.getElementById("hardwareVideoBtn").checked = true;
-    function setVideo(value) {
+    function setVideoByUser(value) {
         if(value === "")
             document.getElementById("videoSection").innerHTML = "";
         else{
             const userchoice = document.getElementById("userVideo").value;
-            const res = "<iframe width=\"400\" height=\"300\" src=\" " + userchoice + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+            let video_id = userchoice.split(\'v=\')[1];
+            let ampersandPosition = video_id.indexOf(\'&\');
+            if(ampersandPosition != -1) {
+                video_id = video_id.substring(0, ampersandPosition);
+            }
+            const videoEmbeddedLink = "https://www.youtube.com/embed/" + video_id;
 
-            document.getElementById("videoSection").innerHTML = res;
+            const res = "<iframe width=\"400\" height=\"300\" src=\" " + videoEmbeddedLink + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+
+            document.getElementById("videoSection2").innerHTML = res;
         }
 
     }
